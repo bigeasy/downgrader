15a31b1b4c02aab0255d377701c085fcbe665179
Alan Gutierrez <alan@prettyrobots.com>
2017-06-03 22:54:03 -0500

    Reduce socket class.

--- socket.js
+++ socket.js
@@ -1,38 +1,17 @@
 var delta = require('delta')
 var cadence = require('cadence')
 var assert = require('assert')
-var coalesce = require('extant')
 
-var PROTOCOL = {
-    http: require('http'),
-    https: require('https')
+exports.headers = function (headers) {
+    headers = JSON.parse(JSON.stringify(headers))
+    headers.connection = 'Upgrade'
+    headers.upgrade = 'Conduit'
+    headers['sec-conduit-protocol-id'] = 'c2845f0d55220303d62fc68e4c145877'
+    headers['sec-conduit-version'] = '1'
+    return headers
 }
 
-exports.connect = cadence(function (async, options) {
-    var protocol = options.secure ? PROTOCOL.https : PROTOCOL.http
-    options = {
-        port: coalesce(options.port, options.secure ? 443 : 80),
-        host: coalesce(options.host, 'localhost'),
-        socketPath: options.socketPath,
-        headers: JSON.parse(JSON.stringify(coalesce(options.headers, {})))
-    }
-    var headers = {
-        connection: 'Upgrade',
-        upgrade: 'Conduit',
-        'sec-conduit-protocol-id': 'c2845f0d55220303d62fc68e4c145877',
-        'sec-conduit-version': 1
-    }
-    for (var name in headers) {
-        options.headers[name] = headers[name]
-    }
-    if (options.socketPath) {
-        delete options.port
-        delete options.host
-        options.headers.host = options.host + ':' + options.port
-    } else {
-        delete options.socketPath
-    }
-    var request = protocol.request(options)
+exports.upgrade = cadence(function (async, request) {
     request.on('response', assert.bind(null, false, 'should be no response'))
     async(function () {
         delta(async()).ee(request).on('upgrade')
--- t/socket.t.js
+++ t/socket.t.js
@@ -18,18 +18,26 @@ function prove (async, assert) {
         assert(request.headers, {
             connection: 'Upgrade',
             upgrade: 'Conduit',
-            host: 'localhost:8088',
+            host: '127.0.0.1:8088',
             'sec-conduit-protocol-id': 'c2845f0d55220303d62fc68e4c145877',
             'sec-conduit-version': '1'
         }, 'request')
         upgrader.upgrade(request, socket, head)
     })
     var socket = require('../socket')
+    var http = require('http')
     assert(socket, 'require')
     async(function () {
         server.listen(8088, async())
     }, function () {
-        socket.connect({ port: 8088 }, async())
+        var request = http.request({
+            host: '127.0.0.1',
+            port: 8088,
+            headers: socket.headers({
+                host: '127.0.0.1:8088'
+            })
+        })
+        socket.upgrade(request, async())
     }, function (request, socket, head) {
         socket.write('hello')
         async(function () {
